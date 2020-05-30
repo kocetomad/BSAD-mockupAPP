8fff82f1491472c3f4de400ef67496ddfe8f63d7
kocetomad <alexander.petrov420@gmail.com>
2020-05-30 22:21:23 +0300

    fixed stef's bugs introduced by his html :(

--- views/LoginForm/controllers/auth.js
+++ views/LoginForm/controllers/auth.js
@@ -1,47 +1,54 @@
-console.log("!!")
+console.log("!!");
 function _login() {
-    users = [
-        "Peter_Williams",
-        "Jane_Fenton",
-        "Juan_Reyes",
-        "Michelle_Ford",
-        "David_Davison",
-        "Moira_Lewis"
-    ]
+  console.log("a");
+  users = [
+    "Peter_Williams",
+    "Jane_Fenton",
+    "Juan_Reyes",
+    "Michelle_Ford",
+    "David_Davison",
+    "Moira_Lewis",
+  ];
 
-    passwords = [
-        "end.user",
-        "service.desk",
-        "major.incidents.manager",
-        "resolver",
-        "queue.manager",
-        "technician"
-    ]
+  passwords = [
+    "end.user",
+    "service.desk",
+    "major.incidents.manager",
+    "resolver",
+    "queue.manager",
+    "technician",
+  ];
 
-    roles = [
-        "EndUser",
-        "ServiceDesk",
-        "MajorIncidentsManager",
-        "Resolver",
-        "QueueManager",
-        "Technician"
+  roles = [
+    "EndUser",
+    "ServiceDesk",
+    "MajorIncidentsManager",
+    "Resolver",
+    "QueueManager",
+    "Technician",
+  ];
+  usr = document.getElementById("loginFormUsername");
+  pas = document.getElementById("loginFormPassword");
 
-    ]
-    usr=document.getElementById("username-login");
-    pas=document.getElementById("password-login");
-
-    if(searchStringInArray(usr.value,users)==searchStringInArray(pas.value,passwords)){
-        location.href="../"+roles[searchStringInArray(pas.value,passwords)]+"/index.html"
+  if (
+    searchStringInArray(usr.value, users) ==
+    searchStringInArray(pas.value, passwords)
+  ) {
+    if (roles[searchStringInArray(pas.value, passwords)]) {
+      location.href =
+        "../" +
+        roles[searchStringInArray(pas.value, passwords)] +
+        "/index.html";
     }
-    
+  }
 }
 
-function searchStringInArray (str, strArray) {
-    for (var j=0; j<strArray.length; j++) {
-        if (strArray[j].localeCompare(str)==0){
-            console.log(j)
-            return j;
-        } 
+function searchStringInArray(str, strArray) {
+  for (var j = 0; j < strArray.length; j++) {
+    if (strArray[j].localeCompare(str) == 0) {
+      console.log(j);
+      return j;
     }
-    return -1;
+  }
+  return -1;
 }
