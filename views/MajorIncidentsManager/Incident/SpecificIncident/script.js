1a30aa0a30c77dcdb17aa347f3d776aa6f2f3af5
TermoZour <stefanpruneanu@gmail.com>
2020-06-03 20:32:32 +0300

    added functionality to SpecificIncident page

--- views/MajorIncidentsManager/Incident/SpecificIncident/script.js
+++ views/MajorIncidentsManager/Incident/SpecificIncident/script.js
@@ -1,46 +1,50 @@
-function elementHide(element, boolean) {
-
-    if (boolean) {
-        // true - hide element
-        element.style.display = "none";
-    } else {
-        // false - un-hide element
-        element.style.display = "block";
-    }
-}
-
-function formLock(form, boolean) {
-    if (boolean) {
-        // true - disable form
-        form.setAttribute("disabled", "");
-    } else {
-        // false - enable form
-        form.removeAttribute("disabled");
-    }
-}
-
-function showHideReasonBox() {
-    if (reasonBoxHidden) {
-        reasonBoxHidden = false;
-        document.getElementById("reasonBox").style = "display: block;";
-    } else {
-        reasonBoxHidden = true;
-        document.getElementById("reasonBox").style = "display: none;";
-    }
-}
-
-function formReturn() {
-    document.getElementById("newIncidentFormDescription").value = " ";
-    document.getElementById("submitText").style.display = "inline";
-    setTimeout(returnToHome, 3000);
-}
+var notes = [{id: 0, time_of_note: '15:00', description: 'some text goes here. some text goes here'}];
 
 function returnToHome() {
     location.href = "../../index.html";
 }
 
+function addNote(newNoteTime, newNoteDetails) {
+    //get last note ID
+    let lastNoteID = notes[notes.length - 1].id;
+    let newNoteID = lastNoteID + 1;
+
+    //prepare the innerHTML for the note template
+    let newNoteInnerHTML = "                <div class=\"card-body\" id=\"" + "note" + newNoteID + "\">\n" +
+        "                    <table class=\"table table-striped table-hover\">\n" +
+        "                        <thead>\n" +
+        "                        <tr class=\"table-primary\">\n" +
+        "                            <th style=\"width: 10%;\" scope=\"col\">ID</th>\n" +
+        "                            <th style=\"width: 10%;\" scope=\"col\">Time of Note</th>\n" +
+        "                            <th style=\"width: 80%;\" scope=\"col\">Details</th>\n" +
+        "                        </tr>\n" +
+        "                        </thead>\n" +
+        "                        <tbody>\n" +
+        "                        <tr>\n" +
+        "                            <th scope=\"row\">" + newNoteID + "</th>\n" +
+        "                            <td>" + newNoteTime + "</td>\n" +
+        "                            <td>" + newNoteDetails + "</td>\n" +
+        "                        </tr>\n" +
+        "                        </tbody>\n" +
+        "                    </table>\n" +
+        "                </div>\n"
+
+    let notesElement = document.getElementById("notes")
+
+    notes.push({id:newNoteID, time_of_note: newNoteTime, description: newNoteDetails});
+
+    notesElement.insertAdjacentHTML("beforeend", newNoteInnerHTML);
+}
+
+function sendNoteData() {
+    let newNoteTime = document.getElementById("newNoteTime").value;
+    let newNoteDescription = document.getElementById("newNoteDescription").value;
+
+    addNote(newNoteTime, newNoteDescription);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     // Execute after page has loaded completely
-
-
+    addNote("11:11", "details1. details1");
+    addNote("11111111:11111111", "details122222. details12222");
 });
