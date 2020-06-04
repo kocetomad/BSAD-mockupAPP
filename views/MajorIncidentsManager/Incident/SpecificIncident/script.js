303b8ba1e51775cf498d79789f6ed995557a50cf
TheJybo <bhtalbot@hotmail.co.uk>
2020-06-04 08:31:00 +0100

    Updated New Note
    Corrected the feature to add the current datetime and user to the new note.

--- views/MajorIncidentsManager/Incident/SpecificIncident/script.js
+++ views/MajorIncidentsManager/Incident/SpecificIncident/script.js
@@ -4,10 +4,12 @@ function returnToHome() {
     location.href = "../../index.html";
 }
 
-function addNote(newNoteTime, newNoteDetails) {
+function addNote(newNoteTime, newNoteDetails, newNoteUser) {
     //get last note ID
     let lastNoteID = notes[notes.length - 1].id;
     let newNoteID = lastNoteID + 1;
+
+    
 
     //prepare the innerHTML for the note template
     let newNoteInnerHTML = "                <div class=\"card-body\" id=\"" + "note" + newNoteID + "\">\n" +
@@ -15,13 +17,15 @@ function addNote(newNoteTime, newNoteDetails) {
         "                        <thead>\n" +
         "                        <tr class=\"table-primary\">\n" +
         "                            <th style=\"width: 10%;\" scope=\"col\">ID</th>\n" +
+        "                            <th style=\"width: 10%;\" scope=\"col\">User</th>\n" +
         "                            <th style=\"width: 10%;\" scope=\"col\">Time of Note</th>\n" +
-        "                            <th style=\"width: 80%;\" scope=\"col\">Details</th>\n" +
+        "                            <th style=\"width: 70%;\" scope=\"col\">Details</th>\n" +
         "                        </tr>\n" +
         "                        </thead>\n" +
         "                        <tbody>\n" +
         "                        <tr>\n" +
         "                            <th scope=\"row\">" + newNoteID + "</th>\n" +
+        "                            <td>" + newNoteUser + "</td>\n" +
         "                            <td>" + newNoteTime + "</td>\n" +
         "                            <td>" + newNoteDetails + "</td>\n" +
         "                        </tr>\n" +
@@ -37,14 +41,19 @@ function addNote(newNoteTime, newNoteDetails) {
 }
 
 function sendNoteData() {
-    let newNoteTime = document.getElementById("newNoteTime").value;
+    var today = new Date();
+    var date = '0'+today.getDate()+'/0'+(today.getMonth()+1)+'/'+today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+
+    let newNoteTime = dateTime;
     let newNoteDescription = document.getElementById("newNoteDescription").value;
 
-    addNote(newNoteTime, newNoteDescription);
+    addNote(newNoteTime, newNoteDescription, "Juan_Reyes");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     // Execute after page has loaded completely
-    addNote("11:11", "details1. details1");
-    addNote("11111111:11111111", "details122222. details12222");
+    addNote("27/05/2020 13:20:04", "Upgrading to Priority 1 due to customer-facing nature of incident.", "David_Davison");
+    addNote("27/05/2020 13:45:04", "Have identified the fault, passing to a technician to implement fix.", "Michelle_Ford");
 });
